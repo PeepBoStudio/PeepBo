c2f74fffe91784f7ddec5da2f255b45cb0917fd0
byunyc0124 <baeyonyk124@gmail.com>
2022-07-06 08:42:35 +0900

    클리커 완료

--- Assets/PeepBo/Resources/Prefabs/RoomMode/Clicker.cs
+++ Assets/PeepBo/Resources/Prefabs/RoomMode/Clicker.cs
@@ -9,24 +9,36 @@ using UnityEngine.UI;
 public class Clicker : MonoBehaviour
 {
     [SerializeField] private int targetCount;
-    [SerializeField] private Button button;
     [SerializeField] private Image fillImage;
 
     int currentCount = 0;
 
     private void Start()
     {
-        button.onClick.AddListener(()=> 
-        { 
+        fillImage.fillAmount = (float)currentCount / targetCount;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             currentCount++;
             fillImage.fillAmount = (float)currentCount / targetCount;
-            if(currentCount == targetCount)
+            if (currentCount == 8)
+            {
+                fillImage.color=new Color(255/255f, 171/255f, 111/255f);
+            }
+            else if (currentCount == 13)
+            {
+                fillImage.color = new Color(255/255f, 111/255f, 111/255f);
+            }
+            else if (currentCount == targetCount)
             {
                 var hideUI = new HideUI { UINames = new List<string> { transform.name } };
                 hideUI.ExecuteAsync().Forget();
 
                 new SwitchToNovelMode { ScriptName = GameManager.Command.ScriptName, Label = GameManager.Command.ClickerName }.ExecuteAsync().Forget();
             }
-        });
+        };
     }
 }
